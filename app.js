b3a636bf280c890b04906eaf94bac5618ba06b72
elliotyou <m9402311@mail.ntust.edu.tw>
2021-05-10 16:39:53 +0800

    add delete function

--- app.js
+++ app.js
@@ -59,6 +59,14 @@ app.post('/records/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/records/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
