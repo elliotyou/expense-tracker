ea219c6c44c33286b27ab9c576be66867e3133b9
elliotyou <m9402311@mail.ntust.edu.tw>
2021-05-11 15:57:00 +0800

    add sumAmount function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,10 +3,21 @@ const router = express.Router()
 
 const Record = require('../../models/record')
 
+const sumAmount = function (records) {
+  let sum = 0
+  records.forEach(record => {
+    sum += record.amount
+  })
+  return sum
+}
+
 router.get('/', (req, res) => {
   Record.find()
     .lean()
-    .then(records => res.render('index', { records }))
+    .then(records => {
+      const totalAmount = sumAmount(records)
+      res.render('index', { records, totalAmount })
+    })
     .catch(error => console.error(error))
 })
 
