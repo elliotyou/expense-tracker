f286fe8b90bfe7837319e944b679ddd9ada72066
elliotyou <m9402311@mail.ntust.edu.tw>
2021-05-12 00:05:26 +0800

    add filter category function in home page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -34,9 +34,14 @@ function generateIconCode(categoryString) {
   return output
 }
 
-
 router.get('/', (req, res) => {
-  Record.find()
+  const category = req.query.category
+  const isNoCategoryQuery = typeof (category) === 'undefined'
+  const isSelectAll = category === 'isAll'
+
+  const recordFind = (isNoCategoryQuery || isSelectAll) ? Record.find() : Record.find({ category })
+
+  return recordFind
     .lean()
     .then(records => {
       const totalAmount = sumAmount(records)
@@ -48,4 +53,18 @@ router.get('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
+router.get('/', (req, res) => {
+  const category = typeof (req.query.category) === 'undefine' ? '' : req.query.category
+
+  Record.find({ "category": { $regex: `${category}`, $options: '$i' } })
+    .lean()
+    .then(records => {
+      const totalAmount = sumAmount(records)
+      records.forEach(record => {
+        record['categoryIconCode'] = generateIconCode(record.category)
+      })
+      res.render('index', { records, totalAmount })
+    })
+    .catch(error => console.error(error))
+})
 module.exports = router
