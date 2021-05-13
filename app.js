791fa562acb579a62f26b04813233af28dc5cc03
elliotyou <m9402311@mail.ntust.edu.tw>
2021-05-13 10:17:35 +0800

    create css file and alternative row effect

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static('public'))
 
 app.use(routes)
 
